aaa2f3a25bcce4d647c46ef746cca40c2361b311
Anand <anand.dhanani10@gmail.com>
2021-09-11 09:57:59 +0530

    Change UI for Current Company/Branch/Year selection

--- src/Dekstop/DiamondTrading/Properties/Resources.Designer.cs
+++ src/Dekstop/DiamondTrading/Properties/Resources.Designer.cs
@@ -73,6 +73,16 @@ namespace DiamondTrading.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap DefaultSelection {
+            get {
+                object obj = ResourceManager.GetObject("DefaultSelection", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap delete_24 {
             get {
                 object obj = ResourceManager.GetObject("delete_24", resourceCulture);
