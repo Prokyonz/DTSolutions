858934a92b741125012ba3689dc1dbc57130e446
Abhishek Bendre <abhishekbedre@hotmail.com>
2021-10-06 21:45:24 +0530

    Added payment repository

---
+++ src/BuildingBlocks/EFCore.Support/EFCore.SQL/Interface/IPaymentMaster.cs
@@ -0,0 +1,17 @@
+﻿using Repository.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCore.SQL.Interface
+{
+    public interface IPaymentMaster
+    {
+        Task<List<GroupPaymentMaster>> GetAllPaymentAsync(Guid companyId, Guid financialYearId);
+        Task<GroupPaymentMaster> AddPaymentAsync(GroupPaymentMaster groupPaymentMaster);
+        Task<GroupPaymentMaster> UpdatePaymentAsync(GroupPaymentMaster groupPaymentMaster);
+        Task<bool> DeletePaymentAsync(Guid groupId);
+
+    }
+}
---
+++ src/BuildingBlocks/EFCore.Support/EFCore.SQL/Repository/PaymentMaster.cs
@@ -0,0 +1,52 @@
+﻿using EFCore.SQL.DBContext;
+using EFCore.SQL.Interface;
+using Microsoft.EntityFrameworkCore;
+using Repository.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCore.SQL.Repository
+{
+    public class PaymentMaster : IPaymentMaster, IDisposable
+    {
+        private readonly DatabaseContext _databaseContext;
+
+        public PaymentMaster()
+        {
+            _databaseContext = new DatabaseContext();
+        }
+        public async Task<GroupPaymentMaster> AddPaymentAsync(GroupPaymentMaster groupPaymentMaster)
+        {
+            if (groupPaymentMaster.Id == null)
+                groupPaymentMaster.Id = Guid.NewGuid();
+
+            await _databaseContext.GroupPaymentMaster.AddAsync(groupPaymentMaster);
+            await _databaseContext.SaveChangesAsync();
+
+            return groupPaymentMaster;
+        }
+
+        public Task<bool> DeletePaymentAsync(Guid groupId)
+        {
+            throw new NotImplementedException();
+        }
+        
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<GroupPaymentMaster>> GetAllPaymentAsync(Guid companyId, Guid financialYearId)
+        {
+            return await _databaseContext.GroupPaymentMaster.Where(w => w.CompanyId == companyId && w.FinancialYearId == financialYearId).Include("PaymentMaster").Include("PaymentDetails").ToListAsync();
+        }
+
+        public Task<GroupPaymentMaster> UpdatePaymentAsync(GroupPaymentMaster groupPaymentMaster)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
