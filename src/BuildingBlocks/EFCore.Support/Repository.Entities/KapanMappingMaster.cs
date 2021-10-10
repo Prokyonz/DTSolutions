4c7b9e0a688a79afa7b28d834bb2c1f1269be41b
Abhishek Bendre <abhishekbedre@hotmail.com>
2021-10-10 19:32:30 +0530

    kapan mapping

---
+++ src/BuildingBlocks/EFCore.Support/Repository.Entities/KapanMappingMaster.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Repository.Entities
+{
+    public class KapanMappingMaster
+    {
+        public int Sr { get; set; }
+        [Key]
+        public Guid Id { get; set; }
+        public Guid PurchaseMasterId { get; set; }
+        public Guid PurchaseDetailsId { get; set; }
+        public Guid KapanId { get; set; }
+        public string SlipId { get; set; }
+
+    }
+}
