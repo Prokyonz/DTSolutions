17e5e6476d5f51fa2d98bf80767c3c5bbfe77499
Abhishek Bendre <abhishekbedre@hotmail.com>
2021-10-15 18:28:49 +0530

    Boil Table and migration

---
+++ src/BuildingBlocks/EFCore.Support/EFCore.SQL/Migrations/20211015123639_AddTableBoilMaster.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EFCore.SQL.Migrations
+{
+    public partial class AddTableBoilMaster : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "BoilMaster",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Sr = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    BoilNo = table.Column<int>(nullable: false),
+                    JangadNo = table.Column<int>(nullable: false),
+                    CompanyId = table.Column<Guid>(nullable: false),
+                    BranchId = table.Column<Guid>(nullable: false),
+                    EntryDate = table.Column<DateTime>(nullable: true),
+                    FinancialId = table.Column<Guid>(nullable: false),
+                    BoilType = table.Column<int>(nullable: false),
+                    KapanId = table.Column<Guid>(nullable: false),
+                    ShapeId = table.Column<Guid>(nullable: false),
+                    SizeId = table.Column<Guid>(nullable: false),
+                    Weight = table.Column<decimal>(type: "decimal(18, 4)", nullable: false),
+                    LossWeight = table.Column<decimal>(type: "decimal(18, 4)", nullable: false),
+                    RejectionWeight = table.Column<decimal>(type: "decimal(18, 4)", nullable: false),
+                    HandOverById = table.Column<Guid>(nullable: false),
+                    HandOverToId = table.Column<Guid>(nullable: false),
+                    SlipNo = table.Column<string>(nullable: true),
+                    BoilCategoy = table.Column<int>(nullable: false),
+                    Remarks = table.Column<string>(nullable: true),
+                    IsDelete = table.Column<bool>(nullable: false),
+                    CreatedDate = table.Column<DateTime>(nullable: false),
+                    UpdatedDate = table.Column<DateTime>(nullable: true),
+                    CreatedBy = table.Column<Guid>(nullable: false),
+                    UpdatedBy = table.Column<Guid>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BoilMaster", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BoilMaster");
+        }
+    }
+}
---
+++ src/BuildingBlocks/EFCore.Support/Repository.Entities/BoilMaster.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Repository.Entities
+{
+    public class BoilMaster
+    {
+        public int Sr { get; set; }
+        [Key]
+        public Guid Id { get; set; }
+        public int BoilNo { get; set; }
+        public int JangadNo { get; set; }
+        public Guid CompanyId { get; set; }
+        public Guid BranchId { get; set; }
+        public DateTime? EntryDate { get; set; }
+        public Guid FinancialId { get; set; }
+        public int BoilType { get; set; } //Send, Receive
+        public Guid KapanId { get; set; }
+        public Guid ShapeId { get; set; }
+        public Guid SizeId { get; set; }
+
+        [Column(TypeName = "decimal(18, 4)")]
+        public decimal Weight { get; set; }
+        [Column(TypeName = "decimal(18, 4)")]
+        public decimal LossWeight { get; set; }
+        [Column(TypeName = "decimal(18, 4)")]
+        public decimal RejectionWeight { get; set; }
+        public Guid HandOverById { get; set; }
+        public Guid HandOverToId { get; set; }        
+        public string SlipNo { get; set; }
+        public int BoilCategoy { get; set; } // 0 -> Received Carats, 1-> Loss Carets -> Rejection Carates
+        public string Remarks { get; set; }
+        public bool IsDelete { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+        public Guid CreatedBy { get; set; }
+        public Guid? UpdatedBy { get; set; }
+    }
+}
