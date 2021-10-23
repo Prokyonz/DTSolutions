30e10558ff4228ea6f146e94dbf7e806195472f9
Abhishek Bendre <abhishekbedre@hotmail.com>
2021-10-23 13:17:40 +0530

    max functon support in salary

--- src/BuildingBlocks/EFCore.Support/EFCore.SQL/Interface/ISalaryMaster.cs
+++ src/BuildingBlocks/EFCore.Support/EFCore.SQL/Interface/ISalaryMaster.cs
@@ -8,6 +8,7 @@ namespace EFCore.SQL.Interface
     public interface ISalaryMaster
     {
         Task<List<SalaryMaster>> GetSalaries(string companyId, string branchId, string financialYear);
+        Task<long> GetMaxSrNoAsync(string companyId, string finanialYearId);
         Task<SalaryMaster> GetSalaries(int month, string companyId, string branchId, string financialYear);
         Task<SalaryMaster> AddSalary(SalaryMaster salaryMaster);
         Task<bool> UpdateSalary(SalaryMaster salaryMaster);
