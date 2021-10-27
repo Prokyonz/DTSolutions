dc7b02475d5d9b16da4deb2e1aed1e22442c5513
Abhishek Bendre <abhishekbedre@hotmail.com>
2021-10-27 18:04:34 +0530

    Update kapan master SP

---
+++ src/BuildingBlocks/EFCore.Support/unitTest/MSUnitTest.EFCore.SQL/KapanMappingMasterUnitTest.cs
@@ -0,0 +1,29 @@
+﻿using EFCore.SQL.Repository;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Repository.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MSUnitTest.EFCore.SQL
+{
+    [TestClass]
+    public class KapanMappingMasterUnitTest
+    {
+        private readonly KapanMappingMasterRepository _kapanMappingMaster;
+
+        public KapanMappingMasterUnitTest()
+        {
+            _kapanMappingMaster = new KapanMappingMasterRepository();
+        }
+
+        [TestMethod]
+        public void GetAllBranchUsingSP()
+        {
+            var data = _kapanMappingMaster.GetPendingKapanMapping("","","");
+            if (data != null)
+                Assert.IsTrue(true);
+            Assert.IsTrue(false);
+        }
+    }
+}
