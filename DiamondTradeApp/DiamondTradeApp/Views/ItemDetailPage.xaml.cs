03206f45491dd7e31e10c29c679f2f0a2aba46b3
Abhishek Bendre <abhishekbedre@hotmail.com>
2022-02-21 18:41:24 +0530

    Mobile application

---
+++ DiamondTradeApp/DiamondTradeApp/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using DiamondTradeApp.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace DiamondTradeApp.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ DiamondTradeApp/DiamondTradeApp/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using DiamondTradeApp.Models;
+using DiamondTradeApp.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DiamondTradeApp.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
