6a7466b2291578d24654238ee4482e06b00f6257
Anand <anand.dhanani10@gmail.com>
2022-04-15 12:18:41 +0530

    Added Code for PriceMaster page changes

---
+++ src/BuildingBlocks/EFCore.Support/Repository.Entities/Model/PriceSPModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Repository.Entities.Model
+{
+    public class PriceSPModel
+    {
+        public string CompanyId { get; set; }
+        public string SizeId { get; set; }
+        public string Size { get; set; }
+        public string NumberId { get; set; }
+        public string Number { get; set; }
+
+        [Column(TypeName = "decimal(18, 4)")]
+        public decimal Price { get; set; }
+    }
+}
