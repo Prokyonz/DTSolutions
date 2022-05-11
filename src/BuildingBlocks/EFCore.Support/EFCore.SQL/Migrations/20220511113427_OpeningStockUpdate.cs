2945de7f0a8289edda64c6174409cf3e8a46d92c
Abhishek Bendre <abhishekbedre@hotmail.com>
2022-05-11 17:05:07 +0530

    Updated

---
+++ src/BuildingBlocks/EFCore.Support/EFCore.SQL/Migrations/20220511113427_OpeningStockUpdate.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EFCore.SQL.Migrations
+{
+    public partial class OpeningStockUpdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NmberId",
+                table: "OpeningStockMaster");
+
+            migrationBuilder.AddColumn<string>(
+                name: "NumberId",
+                table: "OpeningStockMaster",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NumberId",
+                table: "OpeningStockMaster");
+
+            migrationBuilder.AddColumn<string>(
+                name: "NmberId",
+                table: "OpeningStockMaster",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
