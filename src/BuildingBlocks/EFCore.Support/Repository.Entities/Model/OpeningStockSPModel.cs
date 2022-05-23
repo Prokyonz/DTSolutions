f3bc8a05a78197e4fa990ae99239b80c1ec9abd1
Abhishek Bendre <abhishekbedre@hotmail.com>
2022-05-23 19:01:00 +0530

    Opening Stock Reprt MOdel

---
+++ src/BuildingBlocks/EFCore.Support/Repository.Entities/Model/OpeningStockSPModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Repository.Entities.Model
+{
+    public class OpeningStockSPModel
+    {
+        public string Id { get; set; }
+        public int SrNo { get; set; }
+        public string KapanId { get; set; }
+        public string KapanName { get; set; }
+        public string SizeId { get; set; }
+        public string SizeName { get; set; }
+        public string NumberId { get; set; }
+        public string NumberName { get; set; }
+
+        [Column(TypeName = "decimal(18, 4)")]
+        public decimal TotalCts { get; set; }
+        [Column(TypeName = "decimal(18, 4)")]
+        public decimal Rate { get; set; }
+        [Column(TypeName = "decimal(18, 4)")]
+        public decimal Amount { get; set; }
+        public string Remarks { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+        public string FinancialYearId { get; set; }
+        public string CompanyId { get; set; }
+        public string BranchId { get; set; }
+    }
+}
