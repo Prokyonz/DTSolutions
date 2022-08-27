214de2979c537b42532d1358081c9348ba6c2bc3
Hiren Khatri <hirenkhatri1511@gmail.com>
2022-08-27 18:22:11 +0530

    Submit code for make changes on home page boxes as static.

--- DiamondTradeApp/DiamondTradeApp/Views/HomePage.xaml.cs
+++ DiamondTradeApp/DiamondTradeApp/Views/HomePage.xaml.cs
@@ -18,25 +18,41 @@ namespace DiamondTradeApp.Views
         {
             InitializeComponent();
             _reportMasterRepository = new ReportMasterRepository();
-            
+
             FillBox();
-            ReportsView.ItemsSource = reportMastersList;
+            //ReportsView.ItemsSource = reportMastersList;
         }
 
         private void FillBox()
         {
             var reportList = _reportMasterRepository.GetDashboardReports("", "", 0);
+            ReportMaster reportMaster = null;
             if (reportList != null)
             {
-                for (int i = 0; i < reportList.Rows.Count; i++)
+                reportMaster = new ReportMaster
                 {
-                    reportMastersList.Add(new ReportMaster()
-                    {
-                        Type = reportList.Rows[i][0].ToString(),
-                        Component = reportList.Rows[i][1].ToString(),
-                        Value = reportList.Rows[i][2].ToString()
-                    });
-                }
+                    Asset = reportList.Rows[0][2].ToString(),
+                    CashInHand = reportList.Rows[1][2].ToString(),
+                    Deposit = reportList.Rows[2][2].ToString(),
+                    Investment = reportList.Rows[3][2].ToString(),
+                    OpeningStock = reportList.Rows[4][2].ToString(),
+                    Sales = reportList.Rows[5][2].ToString(),
+                    UchinaPlusEmployee = reportList.Rows[6][2].ToString(),
+                    CapitalAccount = reportList.Rows[7][2].ToString(),
+                    Loan = reportList.Rows[8][2].ToString(),
+                    Purchase = reportList.Rows[9][2].ToString()
+                };
+
+                lblAssets.Text = reportMaster.Asset;
+                lblCashInHand.Text = reportMaster.CashInHand;
+                lblCapitalAct.Text = reportMaster.CapitalAccount;
+                lblDeposit.Text = reportMaster.Deposit;
+                lblInvestment.Text = reportMaster.Investment;
+                lblLoan.Text = reportMaster.Loan;
+                lblOpeningStock.Text = reportMaster.OpeningStock;
+                lblPurchase.Text = reportMaster.Purchase;
+                lblSales.Text = reportMaster.Sales;
+                lblUchina.Text = reportMaster.UchinaPlusEmployee;
             }
         }
     }
@@ -47,6 +63,16 @@ namespace DiamondTradeApp.Views
         public string Component { get; set; }
         public string Value { get; set; }
 
+        public string Asset { get; set; }
+        public string CashInHand { get; set; }
+        public string Deposit { get; set; }
+        public string Investment { get; set; }
+        public string OpeningStock { get; set; }
+        public string Sales { get; set; }
+        public string UchinaPlusEmployee { get; set; }
+        public string CapitalAccount { get; set; }
+        public string Loan { get; set; }
+        public string Purchase { get; set; }
     }
 
     public class Employee
