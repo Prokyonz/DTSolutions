050d265177f99f1219c3dab61db13184a4b885db
Akshay Jariwala <jariwala.akshay@prokyonz.com>
2023-04-08 15:04:44 +0530

    CalculatorController added

--- src/Service/DiamondTrade.API/Controllers/CalculatorController.cs
+++ src/Service/DiamondTrade.API/Controllers/CalculatorController.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DiamondTrade.API.Models.Request;
+using EFCore.SQL.Interface;
+using Microsoft.AspNetCore.Mvc;
+using Repository.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace DiamondTrade.API.Controllers
 {
@@ -6,9 +11,74 @@ namespace DiamondTrade.API.Controllers
     [ApiController]
     public class CalculatorController : ControllerBase
     {
-        public CalculatorController()
+        private readonly ICalculatorMaster _calculatorMaster;
+        public CalculatorController(ICalculatorMaster calculatorMaster)
         {
-                
+            _calculatorMaster = calculatorMaster;
+        }
+
+        [Route("Add")]
+        [HttpPost]
+        public async Task<IActionResult> Add([FromBody] CalculatorMaster calculatorData)
+        {
+            try
+            {
+                var result = await _calculatorMaster.AddCalculatorAsync(calculatorData);
+
+                return Ok(result);
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        [Route("Delete")]
+        [HttpPost]
+        public async Task<IActionResult> Delete(int calculatorId)
+        {
+            try
+            {
+                var result = await _calculatorMaster.DeleteCalculatorAsync(calculatorId);
+
+                return Ok(result);
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        [Route("GetAll")]
+        [HttpGet]
+        public async Task<IActionResult> GetAllCaluculator()
+        {
+            try
+            {
+                var result = await _calculatorMaster.GetAllCalculatorAsync();
+
+                return Ok(result);
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        [Route("Update")]
+        [HttpPut]
+        public async Task<IActionResult> Update(List<CalculatorMaster> calculatorList)
+        {
+            try
+            {
+                var result = await _calculatorMaster.UpdateCalculatorAsync(calculatorList);
+
+                return Ok(result);
+            }
+            catch
+            {
+                throw;
+            }
         }
     }
 }
