cb66cd2c689d72187ee9c37fcf194de06c7bc180
Abhishek Bendre <prokyonz.tech@gmail.com>
2023-04-08 17:46:33 +0530

    Migration update for sliptransfer

---
+++ src/BuildingBlocks/EFCore.Support/EFCore.SQL/Migrations/20230408121533_UpdateSlipTable.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EFCore.SQL.Migrations
+{
+    public partial class UpdateSlipTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "BranchId",
+                table: "SlipTransferEntry",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "FinancialYearId",
+                table: "SlipTransferEntry",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "SrNo",
+                table: "SlipTransferEntry",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BranchId",
+                table: "SlipTransferEntry");
+
+            migrationBuilder.DropColumn(
+                name: "FinancialYearId",
+                table: "SlipTransferEntry");
+
+            migrationBuilder.DropColumn(
+                name: "SrNo",
+                table: "SlipTransferEntry");
+        }
+    }
+}
