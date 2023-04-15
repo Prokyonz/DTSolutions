2b3c7688b40c9cfff21ea2c6b2ece2b6db6b89ec
Anand <anand.dhanani10@gmail.com>
2023-04-15 11:47:25 +0530

    Added new column for Total Amount

--- src/BuildingBlocks/EFCore.Support/EFCore.SQL/Migrations/20230415060354_AddNewColumnInSlipTransferEntryTable.Designer.cs
+++ src/BuildingBlocks/EFCore.Support/EFCore.SQL/Migrations/20230415060354_AddNewColumnInSlipTransferEntryTable.Designer.cs
@@ -4,14 +4,16 @@ using EFCore.SQL.DBContext;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace EFCore.SQL.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20230415060354_AddNewColumnInSlipTransferEntryTable")]
+    partial class AddNewColumnInSlipTransferEntryTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
