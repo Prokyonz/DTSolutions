026a8adcffab6e64ef1a907af79e2ea3844f3aae
Anand <anand.dhanani10@gmail.com>
2023-04-29 15:12:01 +0530

    Added new API for GetPurchaseReport

--- src/BuildingBlocks/EFCore.Support/EFCore.SQL/DBContext/DatabaseContext.cs
+++ src/BuildingBlocks/EFCore.Support/EFCore.SQL/DBContext/DatabaseContext.cs
@@ -133,12 +133,17 @@ namespace EFCore.SQL.DBContext
             //optionsBuilder.UseSqlServer("Data Source=103.83.81.7;Initial Catalog=karmajew_DiamondTrading;Persist Security Info=True;User ID=karmajew_DiamondTrading;Password=Karmajew@123;TrustServerCertificate=True;Connection Timeout=120;").EnableSensitiveDataLogging();
             //optionsBuilder.UseSqlServer("Data Source=103.83.81.7;Initial Catalog=karmajew_DiamondTrading;Persist Security Info=True;User ID=karmajew_DiamondTradingDev;Password=Karmajew@123;TrustServerCertificate=True;").EnableSensitiveDataLogging();
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
             //optionsBuilder.UseSqlServer("Data Source=diamondtrading.csfyfhqidvqw.ap-east-1.rds.amazonaws.com;Initial Catalog=diamondtrading;Persist Security Info=True;User ID=admin;Password=Bbgk#2023;TrustServerCertificate=True;Connection Timeout=120;").EnableSensitiveDataLogging();
             optionsBuilder.UseSqlServer("Data Source=103.83.81.7;Initial Catalog=karmajew_DiamondTrading;Persist Security Info=True;User ID=karmajew_DiamondTrading;Password=Karmajew@123;TrustServerCertificate=True;").EnableSensitiveDataLogging();
 >>>>>>> Add new class for calculator
             //optionsBuilder.UseSqlServer("Data Source=103.83.81.7;Initial Catalog=karmajew_DiamondTradingLive;Persist Security Info=True;User ID=karmajew_DiamondTrading;Password=Karmajew@123;TrustServerCertificate=True;").EnableSensitiveDataLogging();
             optionsBuilder.UseSqlServer("Data Source=diamondtrading.csfyfhqidvqw.ap-east-1.rds.amazonaws.com;Initial Catalog=diamondtrading;Persist Security Info=True;User ID=admin;Password=Bbgk#2023;TrustServerCertificate=True;").EnableSensitiveDataLogging();
+=======
+            optionsBuilder.UseSqlServer("Data Source=103.83.81.7;Initial Catalog=karmajew_DiamondTradingLive;Persist Security Info=True;User ID=karmajew_DiamondTrading;Password=Karmajew@123;TrustServerCertificate=True;").EnableSensitiveDataLogging();
+            //optionsBuilder.UseSqlServer("Data Source=diamondtrading.csfyfhqidvqw.ap-east-1.rds.amazonaws.com;Initial Catalog=diamondtrading;Persist Security Info=True;User ID=admin;Password=Bbgk#2023;TrustServerCertificate=True;").EnableSensitiveDataLogging();
+>>>>>>> Added new API for GetPurchaseReport
 =======
             optionsBuilder.UseSqlServer("Data Source=103.83.81.7;Initial Catalog=karmajew_DiamondTradingLive;Persist Security Info=True;User ID=karmajew_DiamondTrading;Password=Karmajew@123;TrustServerCertificate=True;").EnableSensitiveDataLogging();
             //optionsBuilder.UseSqlServer("Data Source=diamondtrading.csfyfhqidvqw.ap-east-1.rds.amazonaws.com;Initial Catalog=diamondtrading;Persist Security Info=True;User ID=admin;Password=Bbgk#2023;TrustServerCertificate=True;").EnableSensitiveDataLogging();
