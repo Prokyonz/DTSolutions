7190e3a30ee86deba9f50b8bd51c30660401d910
Anand <anand.dhanani10@gmail.com>
2023-04-29 16:53:53 +0530

    Added New Columns

---
+++ src/BuildingBlocks/EFCore.Support/EFCore.SQL/Migrations/20230429112258_AddExtraColumnsInTbl.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EFCore.SQL.Migrations
+{
+    public partial class AddExtraColumnsInTbl : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CompanyId",
+                table: "CalculatorMaster",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "FinancialYearId",
+                table: "CalculatorMaster",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CompanyId",
+                table: "CalculatorMaster");
+
+            migrationBuilder.DropColumn(
+                name: "FinancialYearId",
+                table: "CalculatorMaster");
+        }
+    }
+}
