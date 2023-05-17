7f9eafa034b458a666c84ccbf8f51dec0a118f12
Prokyonz Tech <prokyonz.tech@gmail.com>
2023-05-17 17:48:55 +0530

    URL Change

--- src/Service/Trade.Web/src/app/environments.ts
+++ src/Service/Trade.Web/src/app/environments.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    apiURL: "http://192.168.29.223/calculator/api/",
-    //apiURL: "http://65.0.93.228/diamondapi/api/"
+    //apiURL: "http://192.168.29.223/calculator/api/",
+    apiURL: "http://65.0.93.228/diamondapi/api/"
 }
