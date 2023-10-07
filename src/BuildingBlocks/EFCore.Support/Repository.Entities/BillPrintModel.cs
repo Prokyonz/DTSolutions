9b10fea1aa4342be3b726eda7f4f551b53134167
Abhishek Bendre <prokyonz.tech@gmail.com>
2023-10-07 13:17:28 +0530

    Added the AmountInWords column

--- src/BuildingBlocks/EFCore.Support/Repository.Entities/BillPrintModel.cs
+++ src/BuildingBlocks/EFCore.Support/Repository.Entities/BillPrintModel.cs
@@ -62,6 +62,7 @@ namespace Repository.Entities
         public string CompanyId { get; set; }
         public string  BranchId { get; set; }
         public string FinancialYearId { get; set; }
+        public string AmountInWords { get; set; }
 
     }
 }
