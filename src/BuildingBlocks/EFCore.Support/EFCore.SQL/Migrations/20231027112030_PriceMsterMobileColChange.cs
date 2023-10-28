322e93a47293e4b3be6bc2d1fc7586e394994c50
admin <prokyonz.tech@gmail.com>
2023-10-28 11:41:41 +0530

    pricemastermobile form

---
+++ src/BuildingBlocks/EFCore.Support/EFCore.SQL/Migrations/20231027112030_PriceMsterMobileColChange.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EFCore.SQL.Migrations
+{
+    public partial class PriceMsterMobileColChange : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NumberId",
+                table: "PriceMasterMobile");
+
+            migrationBuilder.DropColumn(
+                name: "SizeId",
+                table: "PriceMasterMobile");
+
+            migrationBuilder.AddColumn<string>(
+                name: "NumberName",
+                table: "PriceMasterMobile",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "SizeName",
+                table: "PriceMasterMobile",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NumberName",
+                table: "PriceMasterMobile");
+
+            migrationBuilder.DropColumn(
+                name: "SizeName",
+                table: "PriceMasterMobile");
+
+            migrationBuilder.AddColumn<string>(
+                name: "NumberId",
+                table: "PriceMasterMobile",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "SizeId",
+                table: "PriceMasterMobile",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
