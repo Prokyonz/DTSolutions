22f609974999c792d8df614389b51508cb704468
Abhishek Bendre <prokyonz.tech@gmail.com>
2023-10-28 10:47:21 +0530

    Validation Model Added

---
+++ src/BuildingBlocks/EFCore.Support/Repository.Entities/Model/ValidationSPModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repository.Entities.Model
+{
+    public class ValidationSPModel
+    {
+        public int Status { get; set; }
+    }
+}
