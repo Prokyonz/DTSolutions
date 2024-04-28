e2755eea438e01bb7de2067e74d0fea36422cdee
Abhishek Bendre <prokyonz.tech@gmail.com>
2024-04-28 12:28:40 +0530

    point 33

--- src/Dekstop/DiamondTrading/Transaction/FromChildLedgerReport.cs
+++ src/Dekstop/DiamondTrading/Transaction/FromChildLedgerReport.cs
@@ -11,6 +11,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.UI.WebControls;
 using System.Windows.Forms;
 
 namespace DiamondTrading.Transaction
@@ -49,7 +50,7 @@ namespace DiamondTrading.Transaction
                 try
                 {
                     string v = LedgerId;
-                    GridView view = sender as GridView;
+                    DevExpress.XtraGrid.Views.Grid.GridView view = sender as DevExpress.XtraGrid.Views.Grid.GridView;
                     GridColumnSummaryItem item = e.Item as GridColumnSummaryItem;
                     double Total = double.Parse(view.Columns["Credit"].SummaryText);
                     double saleRate = double.Parse(view.Columns["Debit"].SummaryText);
@@ -72,6 +73,57 @@ namespace DiamondTrading.Transaction
         {
             if (e.KeyCode == Keys.Escape)
                 this.Close();
+        }
+
+        private void exportExcelToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            grvChildLedgerReport.ExpandAllGroups();
+            grvChildLedgerReport.BestFitColumns();
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Excel Files (*.xlsx)|*.xlsx";
+            saveFileDialog.Title = "Save an Excel File";
+            saveFileDialog.ShowDialog();
+
+            if (saveFileDialog.FileName != "")
+                grvChildLedgerReport.ExportToXlsx(saveFileDialog.FileName);
+        }
+
+        private void exportPDFToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "PDF Files (*.pdf)|*.pdf";
+            saveFileDialog.Title = "PDF an Excel File";
+            saveFileDialog.ShowDialog();
+
+            if (saveFileDialog.FileName != "")
+            {
+                this.Cursor = Cursors.WaitCursor;
+                DevExpress.XtraPrinting.PrintingSystem printingSystem1 = new DevExpress.XtraPrinting.PrintingSystem();
+                DevExpress.XtraPrinting.PrintableComponentLink printLink = new DevExpress.XtraPrinting.PrintableComponentLink();
+
+                DevExpress.XtraPrinting.PdfExportOptions options = new DevExpress.XtraPrinting.PdfExportOptions();
+
+                try
+                {
+                    printLink.Component = gridControlChildLedgerReport;
+                    printLink.CreateDocument(printingSystem1);
+                    printLink.Landscape = true;
+
+                    printingSystem1.ShowPrintStatusDialog = true;
+                    printingSystem1.PageSettings.Landscape = true;
+                    printingSystem1.ExportToPdf(saveFileDialog.FileName, options);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                finally
+                {
+                    printingSystem1.Dispose();
+                    printLink.Dispose();
+                    this.Cursor = Cursors.Default;
+                }
+            }
         }
     }
 }
